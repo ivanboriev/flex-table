ec71065d2455f2c48884ffd9cbb2aa5fcf6a919b
Semenkov Maksim <makskharovsk@gmail.com>
2019-11-25 09:58:51 +0300

    замечания по Body

--- src/components/table/body/Body.jsx
+++ src/components/table/body/Body.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from '../panel/Panel';
 import DataTable from '../data-table/DataTable';
-import { cloneDeep } from 'lodash';
+import { cloneDeep } from 'lodash'; // ?: Есть смысл использовать дипклон тут?
 
 class Body extends React.Component {
   constructor(props) {
@@ -13,13 +13,13 @@ class Body extends React.Component {
     };
   }
   flatDataSource = (data = [], parent = {}, visible = true) => {
-    const cloneData = cloneDeep(data);
+    const cloneData = cloneDeep(data); // ?: Есть смысл использовать дипклон тут?
     return cloneData.reduce((acc, item) => {
       if (Object.keys(parent).length > 0) {
-        item.parentId = parent.id;
-        item.id = `${item.id}-${parent.id}`;
+        item.parentId = parent.id; // ?: мутация
+        item.id = `${item.id}-${parent.id}`;  // ?: мутация
       } else {
-        item.parentId = null;
+        item.parentId = null; // ?: мутация
       }
       if (item.categories) {
         return [
@@ -29,7 +29,7 @@ class Body extends React.Component {
             categories: null,
             hasChilds: true,
             visible: visible,
-            class: 'category',
+            class: 'category',  // ?: class - зарезервированное слово, лучше className
             expanded: false,
           },
           ...this.flatDataSource(item.categories, item, false),
@@ -44,7 +44,7 @@ class Body extends React.Component {
             hasChilds: true,
             items: null,
             visible: visible,
-            class: 'item',
+            class: 'item', // ?: class - зарезервированное слово, лучше className
             expanded: false,
           },
           ...this.flatDataSource(item.items, item, false),
@@ -71,27 +71,27 @@ class Body extends React.Component {
 
   toggleElement(element) {
     const { data } = this.state;
-    const isExpand = (element.expanded = !element.expanded);
+    const isExpand = (element.expanded = !element.expanded); // ?: мутация, плюс неочевидно, что происходит, возможно переписать?
     const parents = [];
     if (element.hasChilds) {
-      parents.push(element.id);
+      parents.push(element.id); // ?: возможно написать метод по получению парентов? слишком большая ответственность у метода, плюс мутации.
     }
 
     return data.map(el => {
       if (isExpand) {
-        if (parents.indexOf(el.parentId) !== -1) {
-          el.visible = true;
+        if (parents.indexOf(el.parentId) !== -1) { // ?: parents.includes(el.parentId)
+          el.visible = true; // ?: мутация
         }
-      } else {
-        if (parents.indexOf(el.parentId) !== -1) {
-          el.visible = false;
-          el.expanded = false;
+      } else { // ?: тут можно обойтись ранним ретерном, чем меньше вложенность, тем лучше.
+        if (parents.indexOf(el.parentId) !== -1) { // ?: parents.includes(el.parentId)
+          el.visible = false; // ?: мутация
+          el.expanded = false; // ?: мутация
           if (el.hasChilds) {
-            parents.push(el.id);
+            parents.push(el.id); // ?: Зачем на этом этапе заполняется массив парентов? он больше нигде не используестся
           }
         }
       }
-      return el;
+      return el; // ?: можно избавиться, за счет раннего ретерна
     });
   }
 
@@ -107,10 +107,10 @@ class Body extends React.Component {
     return (
       <div className="body" key="key">
         {filteredData.map(el => {
-          const expanded = el.expanded;
-          const hasChilds = el.hasChilds;
+          const expanded = el.expanded; // ?: тут, в принципе, можно не записывать в переменную значения, но если писать, то через деструктуризацию в данном случае
+          const hasChilds = el.hasChilds; // ?: тут, в принципе, можно не записывать в переменную значения, но если писать, то через деструктуризацию в данном случае
           return hasChilds ? (
-            <Panel
+            <Panel // ?: ExpandingRow или ContainerRow или еще что-то семантически более близкое к тому что делает строка
               key={el.id}
               title={el.name}
               onClick={() => this.handleClick(el)}
